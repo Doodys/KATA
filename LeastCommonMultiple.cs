2d860aa1d5701c3e184be8b4184394f05277c09d
Majkel Dudyselo <dudys.michal@gmail.com>
2018-03-08 14:48:49 +0100

    Will it work?

---
+++ LeastCommonMultiple.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeastCommonMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 2, 3, 4 };
+            Console.WriteLine(LeastCommonMultipleAggregate(list));
+        }
+        public static int LeastCommonMultipleAggregate(List<int> nums)
+        {
+            if (!nums.Any()) { return 1; }
+            else { return nums.Aggregate(LeastCommonMultiple); }
+        }
+        static int LeastCommonMultiple(int a, int b)
+        {
+            return Math.Abs(a * b) / GreatestCommonDivisor(a, b);
+        }
+        static int GreatestCommonDivisor(int a, int b)
+        {
+            return b == 0 ? a : GreatestCommonDivisor(b, a % b);
+        }
+    }
+}
