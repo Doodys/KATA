4861315cab83df51ae1b8c85f3d0ab40a3a5b048
Dudys <dudys.michal@gmail.com>
2018-03-08 12:36:41 +0100

    Add files via upload

---
+++ DataBase(my own super complicated error handling).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza_Danych
+{
+    public struct Contacts
+    {
+        public string x;
+        public string y;
+        public string z;
+        public string v;
+        public int f;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
