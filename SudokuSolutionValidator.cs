1ada60ab873bc2941b31c6d0d7cc63c1abc2532b
Majkel Dudyselo <dudys.michal@gmail.com>
2018-03-12 23:52:03 +0100

    Sudoku Solution Validator
    Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
    
    Function validSolution accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
    
    The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.

---
+++ SudokuSolutionValidator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolutionValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] Sudoku = new int[][]
+            {
+              new int[] {5, 3, 4, 6, 7, 8, 9, 1, 2},
+              new int[] {6, 7, 2, 1, 9, 5, 3, 4, 8},
+              new int[] {1, 9, 8, 3, 4, 2, 5, 6, 7},
+              new int[] {8, 5, 9, 7, 6, 1, 4, 2, 3},
+              new int[] {4, 2, 6, 8, 5, 3, 7, 9, 1},
+              new int[] {7, 1, 3, 9, 2, 4, 8, 5, 6},
+              new int[] {9, 6, 1, 5, 3, 7, 2, 8, 4},
+              new int[] {2, 8, 7, 4, 1, 9, 6, 3, 5},
+              new int[] {3, 4, 5, 2, 8, 6, 1, 7, 9},
+            }; //true
+            Console.WriteLine(ValidateSolution(Sudoku));
+        }
+        public static bool ValidateSolution(int[][] board)
+        {
+            int x = 0, y = 0, z = 3, v = 3;
+
+            for (int k = 0; k < 9; k++)
+            {
+                for (int i = 0; i < 9; i++)
+                    for (int j = 1; j < 10; j++)
+                        if (board[k][i] == j) { x++; y += board[k][i]; }
+                        else continue;
+                if (x == 9 && y == 45) { x -= 9; y -= 45; continue; }
+                else { return false; }
+            }
+
+            for (int k = 0; k < 9; k++)
+            {
+                for (int i = 0; i < 9; i++)
+                    for (int j = 1; j < 10; j++)
+                        if (board[i][k] == j) { x++; y += board[i][k]; }
+                        else continue;
+                if (x == 9 && y == 45) { x -= 9; y -= 45; continue; }
+                else { return false; }
+            }
+
+            for (int k = 0; k < 9; k++)
+            {
+                if (k == 3 || k == 6) { z += 3; v += 3; }
+                for (int i = z - 3; i < z; i++)
+                    for (int j = v - 3; j < v; j++)
+                        for (int l = 1; l < 10; l++)
+                            if (board[i][j] == l) { x++; y += board[i][j]; }
+                if (x == 9 && y == 45) { x -= 9; y -= 45;}
+                else { return false; }
+            }
+            return true;
+        }
+    }
+}
