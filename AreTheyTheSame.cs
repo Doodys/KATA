b390e1636c8daafa7da7fa403183bc43427d0322
Majkel Dudyselo <dudys.michal@gmail.com>
2018-03-12 11:35:00 +0100

    Are those arrays same? See description.
    Given two arrays a and b write a function comp(a, b) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
    
    Valid arrays:
    a = [121, 144, 19, 161, 19, 144, 19, 11]
    b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
    
    comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
    
    a = [121, 144, 19, 161, 19, 144, 19, 11]
    b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
    
    If a or b are null, the problem doesn't make sense so return false.

---
+++ AreTheyTheSame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreTheyTheSame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 2, 2, 3 };
+            int[] b = new int[] { 4, 9, 9 };
+            bool r = Comp(a, b);
+            Console.WriteLine(r);
+        }
+
+        public static bool Comp(int[] a, int[] b)
+        {
+            foreach (int Number in a) { Console.Write(Number + " "); Console.WriteLine(); }
+            foreach (int Number in b) { Console.Write(Number + " "); }
+            if (a.Length == 0 || b.Length == 0 || a == null || b == null) { return false; }
+            else if (a.Length == b.Length)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Array.Sort(b);
+                    if (IsPresent(b, (a[i] * a[i])) == true) { a[i] -= a[i]; continue; }
+                    else return false;
+                }
+            }
+            return true;
+        }
+        public static bool IsPresent(int[] Vector, int Number)
+        {
+            int Left = 0, Right = Vector.Length - 1, Middle;
+            while (Left <= Right)
+            {
+                Middle = (Left + Right) / 2;
+                if (Vector[Middle] == Number) { Vector[Middle] = 0; return true; }
+                else if (Vector[Middle] > Number) Right = Middle - 1;
+                else Left = Middle + 1;
+            }
+            
+            return false;
+        }
+    }
+}
