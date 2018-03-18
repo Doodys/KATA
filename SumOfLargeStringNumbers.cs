9fc7c218f2c8e8475722495f6b087920223179d7
Majkel Dudyselo <dudys.michal@gmail.com>
2018-03-18 14:00:35 +0100

    Sum of large numbers
    Example of using Sytem.Numerics

---
+++ SumOfLargeStringNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+
+namespace SumOfStringNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "759483679423876984679287593746589743986", b = "98679679546798546987438967489674679346794";
+            Console.WriteLine(Add(a, b));
+        }
+
+        public static string Add(string a, string b)
+        {
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+    }
+}
